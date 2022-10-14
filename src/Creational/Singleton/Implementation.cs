3590d39eca69c663ea8d5cb40c1d539e2ce5d286
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-14 01:43:49 -0300

    feat: Singleton Pattern

---
+++ src/Creational/Singleton/Implementation.cs
@@ -0,0 +1,43 @@
+﻿namespace Singleton
+{
+    /* 
+    Singleton Pattern tem como objetivo garantir que apenas 1 instancia do objeto estaja sendo executada,
+
+    Consequencias:
+    -Encapsula sua unica instancia
+    -Controle sobre quando/como os clientes acessam a instancia
+    -Alternativa melhor a váriaveis globais
+    -Viola o padrão de responsabilidade única, o objeto é responsavel pela criacao e por
+    gerenciar o seu ciclo de vida (desvantagem) 
+    */
+
+    public class Logger
+    {
+        //sempre recomendado inicializacao com LazyLoad para garantir Thread-safe
+        private static readonly Lazy<Logger> _lazyLogger =
+            new Lazy<Logger>(() => new Logger());
+
+        //private static Logger? _instance;
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+
+                //if (_instance is null)
+                //    _instance = new Logger();
+
+                //return _instance;
+            }
+        }
+
+        protected Logger() { }
+
+        //SingletonOperation
+        public void Log(string mensagem)
+        {
+            Console.WriteLine($"Mensagem do log: {mensagem}");
+        }
+    }
+}
---
+++ src/Creational/Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using Singleton;
+
+Console.Title = "Singleton Pattern";
+
+//Não é possivel iniciar um nova instancia a qualquer momente devido o ctor protected
+//var teste = new Logger();
+
+var instancia1 = Logger.Instance;
+var instancia2 = Logger.Instance;
+
+if (instancia1 == instancia2 && instancia2 == Logger.Instance)
+{
+    Console.WriteLine("As instancias sao identicas!");
+}
+
+instancia1.Log($"Mensagem de {nameof(instancia1)}");
+instancia1.Log($"Mensagem de {nameof(instancia2)}");
+Logger.Instance.Log($"Mensagem de {nameof(Logger.Instance)}");
+
+Console.ReadLine();
